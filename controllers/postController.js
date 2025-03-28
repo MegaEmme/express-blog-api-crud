5ac07a682af24af062b708c9b1279318695e00bb
MegaEmme <mikmusco@gmail.com>
2025-03-28 13:50:06 +0100

    create controller function (+ bonus.2)

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+function index (req,res){
+    res.json(posts);
+};
+
+function show (req,res){
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+    if(!post){
+        return res.status(404).json({
+            error: 'Not Found',
+            message: 'Post non trovato'
+        });
+    };
+    res.json(post);
+};
+
+function store (req,res){
+    res.send('Creazione nuovo dolce');
+};
+
+function update (req,res){
+    const {id} = req.params;
+    res.send('Modifica parziale del dolce ' + id);
+};
+
+function modify (req,res){
+    const {id} = req.params;
+    res.send('Modifica parziale del dolce ' + id);
+};
+
+function destroy (req,res){
+    const {id} = req.params
+    res.send('Eliminazione del dolce ' + id);
+};
