8ec5c5ab2039466077dbf0109f9877c910494001
MegaEmme <mikmusco@gmail.com>
2025-04-01 12:34:41 +0200

    feat: create errorsHandler middleware

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const app = express();
 const port = 7000;
 
 const postsRouter = require('./routers/postsRouter');
+
+const errorsHandler = require('./middlewares/errorsHandler');
 //registro gli assets statici
 app.use(express.static('public'));
 //registro il body parser per application json
 app.use(express.json());
 //resgistro la path in cui posizionare il router
 app.use('/posts', postsRouter);
+//registro il middleware errorsHandler
+app.use(errorsHandler);
 
 app.get('/', (req,res)=>{
     res.send('<h1>Il mio server</h1>');
 });
+
+
 //attivo il server sulla porta
 app.listen(port, ()=> {
     console.log(`sono un server attivo sulla porta ${port}`);
